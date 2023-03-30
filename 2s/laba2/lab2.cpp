1abbe968ed15f0967012f841e8845dc59c6ba5ac
Sant1agoAm1go <thesuperhero888@gmail.com>
2023-03-31 02:28:41 +0300

    start lab2

---
+++ 2s/laba2/lab2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "DynamicArray.h"
+#include <stdlib.h>
+#include <time.h>
+using namespace std; 
+int main() {
+    srand(time(NULL));
+    int lenght = 10;
+    DynamicArray<int> data = DynamicArray<int>(lenght);
+    for(int i = 0; i < lenght; i++) {
+        data.Set(i, rand () % 100);
+        cout << data.Get(i) << endl;
+    }
+    return 0;
+}
