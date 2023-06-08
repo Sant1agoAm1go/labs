47028e6f7ee88a7f7ea8e6ee3c8af88a7e55d794
Sant1agoAm1go <thesuperhero888@gmail.com>
2023-06-08 20:22:19 +0300

    update

--- 2s/lab4/Tree.h
+++ 2s/lab4/Tree.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "Sequence.h"
-template <typename Tkey, typename Tdata> struct Node {
+#include <concepts> 
+#include "Complex.h"
+template< class Tkey>
+concept SameHelper = std::same_as<Tkey, int> || std::same_as<Tkey, double> || std::same_as<Tkey, float> ||  std::same_as<Tkey, complex>;
+ 
+template <SameHelper Tkey, typename Tdata> struct Node {
 public:
     Tkey key;
     Tdata data;
