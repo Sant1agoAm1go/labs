8329a6821fc026b5e4235dc2677a299d37418a6a
Sant1agoAm1go <thesuperhero888@gmail.com>
2023-08-25 16:40:40 +0300

    update

--- 3s/lab1/time_test.cpp
+++ 3s/lab1/time_test.cpp
@@ -6,15 +6,12 @@
 #include <time.h>
 int main() {
     srand(time(nullptr));
-    int len = 100000000;
+    int len = 1000000;
     int* data = new int[len];
-    UnqPtr<int>* arr = new UnqPtr<int>[len];
-    for(int i = 0; i < len; i++) {
-        data[i] = rand() % 100;
-    }
     clock_t start = clock();
     for(int i = 0; i < len; i++) {
-        arr[i] = UnqPtr<int>(&data[i]);
+        data[i] = rand() % 100;
+        UnqPtr<int>(&data[i]);
     }
     double end = (double) (clock() - start) / CLOCKS_PER_SEC;
     std::cout << end << std::endl;      
