43e1552c11d8f9cb55a4464d8cbe129311511b36
Sant1agoAm1go <thesuperhero888@gmail.com>
2023-09-17 23:26:59 +0300

    update

--- 3s/lab2/HeapSort.h
+++ 3s/lab2/HeapSort.h
@@ -13,32 +13,32 @@ void Swap(T& a, T& b) {
 template <typename T> 
 class HeapSorter : public ISorter <T> {
     private:
-        void Heapify(Sequence<T>* seq, int (*cmp)(const T&, const T&), int n, int i) {
+        void Heapify(Sequence<T>* seq, int (*cmp)(const T&, const T&), int len, int i) {
             int largest = i;   
             int l = 2*i + 1; 
             int r = 2*i + 2; 
 
-            if (l < n && cmp(seq->Get(l),seq->Get(largest)) > 0)
+            if (l < len && cmp(seq->Get(l),seq->Get(largest)) > 0)
                 largest = l;
-            if (r < n && cmp(seq->Get(r),seq->Get(largest)) > 0)
+            if (r < len && cmp(seq->Get(r),seq->Get(largest)) > 0)
                 largest = r;
 
             if (largest != i) {
                 Swap(seq->Get(i), seq->Get(largest));
-                Heapify(seq, cmp, n, largest);
+                Heapify(seq, cmp, len, largest);
             }
         }
-        void HeapSort(Sequence<T>* seq, int (*cmp)(const T&, const T&), int n) {
-            for (int i = n / 2 - 1; i >= 0; i--)
-                Heapify(seq, cmp, n, i);
-            for (int i=n-1; i>=0; i--) {
+        void HeapSort(Sequence<T>* seq, int (*cmp)(const T&, const T&), int len) {
+            for (int i = len / 2 - 1; i >= 0; i--)
+                Heapify(seq, cmp, len, i);
+            for (int i=len-1; i>=0; i--) {
                 Swap(seq->Get(0), seq->Get(i));
                 Heapify(seq, cmp, i, 0);
             }
         }
     public:
         Sequence<T>* Sort(Sequence<T>* seq, int (*cmp)(const T&, const T&)) {
-            Sequence<T>* result = seq->GetSubsequence(0, seq->GetLength());
+            Sequence<T>* result = seq->GetSubsequence(0, seq->GetLength() - 1);
             HeapSort(result, cmp, result->GetLength());
             return result;
         }   
--- 3s/lab2/HeapSortTest.cpp
+++ 3s/lab2/HeapSortTest.cpp
@@ -16,11 +16,18 @@ int cmp_double_rev(const double& p1, const double& p2) {
     return (p1 - p2 >= 0) ? -1 : 1;
 }
 int main() {
-    int data[] = {1,3,2,5,4};
+    int len;
+    std::cin >> len;
+    std::cin.clear();
+    UnqPtr<int> data = new int[len];
+    for(int i = 0; i < len; i++) {
+        data[i] = rand() % 100;
+    }
     UnqPtr<ISorter<int>> sorter (new HeapSorter<int>);
-    Sequence<int>* seq = new ArraySequence<int>(data, sizeof(data)/sizeof(int));
-    std::cout << seq << std::endl;
-    Sequence<int>* result = sorter->Sort(seq, cmp_int_rev);
-    //std::cout << result << std::endl;
+    UnqPtr<Sequence<int>> seq = new ArraySequence<int>(data.Get(), len);
+    std::cout << "Before sort:" << seq.Get() << std::endl;
+    Sequence<int>* result = sorter->Sort(seq.Get(), cmp_int_rev);
+    std::cout << "After sort:" << result << std::endl;
+    std::cout << "Everything is fine" << std::endl;
     return 0;
 }
